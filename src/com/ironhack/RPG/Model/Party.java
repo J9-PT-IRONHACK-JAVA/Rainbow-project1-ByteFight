50ee1e6c2c4af1bc9a659dab2e2733636554e906
AdrianTrigo <atrigo@outlook.com>
2022-11-02 19:45:18 +0100

    Amended errors in authomatic party generator

--- src/com/ironhack/RPG/Model/Party.java
+++ src/com/ironhack/RPG/Model/Party.java
@@ -9,30 +9,34 @@ public class Party {
     private String partyName;
     private ArrayList<Character> listCharacters;
 
+    private int numTeams;
+
 
     //Main Constructor
     public Party(String partyName) {
+
         setPartyName(partyName);
     }
 
     //Overload Constructor
     public Party() {
-        setPartyName("Team1");
+
+        setPartyName("Team" + numTeams);
     }
 
     //Party automatic constructor
     public void authomaticParty(int partySize) {
         Random ran = new Random();
-        int randomWarriroOrWizard = ran.nextInt(2);
         for (int y = 0; y < partySize; y++) {
-            if (randomWarriroOrWizard == 1) {
+            int randomWarriorOrWizard = ran.nextInt(2);
+            if (randomWarriorOrWizard == 1) {
                 var warrior = new Warrior();//generate a random warrior
-                warrior = (Warrior) checkName(warrior,listCharacters);
+                warrior = (Warrior) checkName(warrior);
                 listCharacters.add(warrior);
 
-            } else if (randomWarriroOrWizard == 0) { //Generate a random Wizard
+            } else if (randomWarriorOrWizard == 0) { //Generate a random Wizard
                 var wizard = new Wizard();
-                wizard = (Wizard) checkName(wizard,listCharacters);
+                wizard = (Wizard) checkName(wizard);
                 listCharacters.add(wizard);
             }
 
@@ -42,16 +46,16 @@ public class Party {
     public void authomaticParty() {
         int partySize = 10;
         Random ran = new Random();
-        int randomWarriroOrWizard = ran.nextInt(2);
         for (int y = 0; y < partySize; y++) {
+            int randomWarriroOrWizard = ran.nextInt(2);
             if (randomWarriroOrWizard == 1) {
                 var warrior = new Warrior();//generate a random warrior
-                warrior = (Warrior) checkName(warrior,listCharacters);
+                warrior = (Warrior) checkName(warrior);
                 listCharacters.add(warrior);
 
             } else if (randomWarriroOrWizard == 0) { //Generate a random Wizard
                 var wizard = new Wizard();
-                wizard = (Wizard) checkName(wizard,listCharacters);
+                wizard = (Wizard) checkName(wizard);
                 listCharacters.add(wizard);
             }
 
@@ -97,22 +101,22 @@ public class Party {
 
 
     //Método que compara el nombre del parametro con los nombres de la lista, si coincide devuelve TRUE
-    public static boolean containsName(String name, ArrayList<Character> listCharacters){
-        for(Character character : listCharacters){
+    public boolean containsName(String name){
+        for(Character character : this.listCharacters){
             if (character.getName() == name){return true;}
             else{ return false;}
         }
         return false;
     }
 
-    public static Character checkName(Character character, ArrayList<Character> listCharacters) {
+    public Character checkName(Character character) {
         do{
-            if (containsName(character.getName(), listCharacters) && containsName(character.getName() + " Jr", listCharacters)) {
+            if (containsName(character.getName()) && containsName(character.getName() + " Jr")) {
                 character.setName(character.nameGenerator());
-            } else if (containsName(character.getName(), listCharacters)) {
+            } else if (containsName(character.getName())) {
                 character.setName(character.getName() + " Jr");
             }
-        } while ((containsName(character.getName(), listCharacters) || containsName(character.getName() + " Jr", listCharacters)));
+        } while ((containsName(character.getName()) || containsName(character.getName() + " Jr")));
         return character;
     }
 
