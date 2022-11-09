0e8f6e76e37c3bb014a95507f5f262f44d59d15d
AdrianTrigo <atrigo@outlook.com>
2022-11-09 17:50:39 +0100

    Improved duel, correction of errors in Party. Created empty displayWinnerAndLooser method in DuelLog

--- src/com/ironhack/RPG/Services/Duel.java
+++ src/com/ironhack/RPG/Services/Duel.java
@@ -5,11 +5,9 @@ import com.ironhack.RPG.Model.Character;
 
 //Servicio en el que se implementa la lógica del duelo.
 public class Duel {
-    private static int round;
-    //Num ronda
+    private static int round; //Num ronda
+
     ////Dos objetos tipo Character que se batirán en duelo
-
-
     public static void duel(Character character1, Character character2) throws InterruptedException {
         round = 1;
         DuelLog duelLog = new DuelLog();
